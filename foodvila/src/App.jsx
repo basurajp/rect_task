bd739927f2091465aa304bf913c917dafb3b6ff4
Basuraj <basurajp135@gmail.com>
2023-12-31 18:46:31 +0530

    food villa basic version done

--- foodvila/src/App.jsx
+++ foodvila/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Header from './components/Header'
-import Body from './components/Body'
+import React, { useEffect } from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { fetchData } from "./utlis/mockData";
 
 const App = () => {
+  useEffect(fetchData);
   return (
-    <div className='main_conatiner'>
-    <Header />
-    <Body />
+    <div className="main_conatiner">
+      <Header />
+      <Body />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
