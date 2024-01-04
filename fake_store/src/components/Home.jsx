91c0f321ba6ce8dc9b6a38f485a26342a8ad9732
Basuraj <basurajp135@gmail.com>
2024-01-04 17:58:20 +0530

    categeory option added

--- fake_store/src/components/Home.jsx
+++ fake_store/src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="w-[85%] h-[full]  p-10 pt-[5%] flex flex-wrap overflow-y-auto overflow-x-hidden">
         {products.map((p, i) => {
           return (
-            <Link
+            <Link key={p.id}
               to={`/details/${p.id}`}
               className="card p-3 border shadow rounded w-[18%]  flex justify-center items-center flex-col mr-3 mb-3 "
             >
--- fake_store/src/components/Navbar.jsx
+++ fake_store/src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { productsContext } from "../utlis/Context";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [products] = useContext(productsContext);
+
+  let distintCategory =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+
+  distintCategory = [...new Set(distintCategory)];
+
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()},${(
+      Math.random() * 255
+    ).toFixed()},${(Math.random() * 255).toFixed()},0.4)`;
+  };
+
+  console.log(color());
+
   return (
     <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center  pt-5">
       <a
@@ -11,24 +28,17 @@ const Navbar = () => {
       </a>
       <hr className="w-[80%] my-3" />
       <h1 className="text-2xl font-bold mb-3 w-[80%] ">Categeory Filter </h1>
-      <ul className=" w-[80%] flex justify-center flex-col">
-        <li className="mb-3 flex items-center ">
-          <span className="span w-4 h-4 bg-red-300 mr-2 rounded-full inline-block"></span>
-          Cat 1
-        </li>
-        <li className="mb-3 flex items-center ">
-          <span className="span w-4 h-4 bg-green-300 mr-2 rounded-full inline-block"></span>
-          Cat 1
-        </li>
-        <li className="mb-3 flex items-center ">
-          <span className="span w-4 h-4 bg-blue-300 mr-2 rounded-full inline-block"></span>
-          Cat 1
-        </li>
-        <li className="mb-3 flex items-center ">
-          <span className="span w-4 h-4 bg-zinc-300 mr-2 rounded-full inline-block"></span>
-          Cat 1
-        </li>
-      </ul>
+      <div className=" w-[80%] flex justify-center flex-col">
+        {distintCategory.map((c, i) => (
+          <Link to={`/?category=${c}`} className="mb-3 flex items-center ">
+            <span
+              style={{ backgroundColor: color() }}
+              className="span w-4 h-4  mr-2 rounded-full inline-block"
+            ></span>
+            {c}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 };
